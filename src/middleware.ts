2a75539ba288060f9ba037ef9ae7572ad3fc69a9
ZikXewen <datasup12@gmail.com>
2022-08-05 13:41:40 +0700

    increase cookie age

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,9 @@ import { type NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest) {
   if (req.cookies.get('pollossal-token')) return
   const res = NextResponse.next()
-  res.cookies.set('pollossal-token', nanoid(), { sameSite: 'strict' })
+  res.cookies.set('pollossal-token', nanoid(), {
+    sameSite: 'strict',
+    maxAge: 3000000,
+  })
   return res
 }
